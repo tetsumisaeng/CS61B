7e04f3df36627b335ccfff485b94eddf5b59a129
tetsumisaeng <zherui.lin@foxmail.com>
2021-06-11 17:02:22 +0800

    done with lab4

--- lab4/flik/TestFlik.java
+++ lab4/flik/TestFlik.java
@@ -1,2 +1,14 @@
-package flik;public class testFlik {
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestFlik {
+
+    @Test
+    public void TestFlik1() {
+        assertTrue(Flik.isSameNumber(0, 0));
+        assertTrue(Flik.isSameNumber(1, 1));
+        assertTrue(Flik.isSameNumber(-1, -1));
+        assertTrue(Flik.isSameNumber(1024, 1024));
+    }
 }
